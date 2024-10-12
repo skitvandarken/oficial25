fc64221b8ecbd758e68c6a42cd04af8fcbb7d569
Pedro Jorge <skitvandarken2013@gmail.com>
2024-10-12 21:45:21 +0100

    site concluido

--- frontend/src/app/paginas/historia/historia.component.ts
+++ frontend/src/app/paginas/historia/historia.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../layout/navbar/navbar.component';
 import { FooterComponent } from '../../layout/footer/footer.component';
+import { Slider2Component } from '../../layout/slider2/slider2.component';
 
 @Component({
   selector: 'app-historia',
   standalone: true,
   imports: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    Slider2Component
   ],
   templateUrl: './historia.component.html',
   styleUrl: './historia.component.css'
