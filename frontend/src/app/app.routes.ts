8341a00a8314215c9af974fae55d3231190aeba3
Lukeba Jorge <lukeba.jorge@angolacables.co.ao>
2025-01-03 16:08:14 +0100

    tudo funcinonando e integração dos planos de hospedagem funcionando

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './paginas/inicio/inicio.component';
 import { ServicosComponent } from './paginas/servicos/servicos.component';
 import { HistoriaComponent } from './paginas/historia/historia.component';
+import { VerificadorComponent } from './layout/verificador/verificador.component';
 
 export const routes: Routes = [
     {path:'', component: InicioComponent},
     {path:'inicio', component: InicioComponent},
     {path:'servicos', component: ServicosComponent},
-    {path:'historia', component: HistoriaComponent}
+    {path:'historia', component: HistoriaComponent},
+    {path: 'verificador', component: VerificadorComponent}
     
 ];
